81619d31fb5006b2154b60d40f74133d73d38753
Alan Gawthorpe <agawthorpe@gmail.com>
2017-04-17 07:51:58 +0100

    Support for wiki folders Plus fix for lists And change to wiki words to use double bracket

---
+++ nav.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+type wikiNav struct {
+	Name   string
+	IsDir  bool
+	SubNav []wikiNav
+}
+type nav struct {
+	Wikis []wikiNav
+	Tags  TagIndex
+}
+
+type byModTime []os.FileInfo
+
+func (m byModTime) Len() int           { return len(m) }
+func (m byModTime) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+func (m byModTime) Less(i, j int) bool { return m[i].ModTime().Before(m[j].ModTime()) }
+
+func contains(target string, in []string) bool {
+	for _, d := range in {
+		if target == d {
+			return true
+		}
+	}
+	return false
+}
+
+func closureProcess(root string, names *[]wikiNav) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		if info.IsDir() && contains(info.Name(), specialDir) {
+			return filepath.SkipDir
+		}
+		// Ignore anything that isnt an md file
+		if strings.HasSuffix(info.Name(), ".md") {
+			tmp := wikiNav{
+				Name: strings.Replace(strings.TrimSuffix(path, ".md"), root, "", 1),
+			}
+			*names = append(*names, tmp)
+		}
+		if strings.HasSuffix(info.Name(), ".pdf") {
+			tmp := wikiNav{
+				Name: strings.Replace(path, root, "", 1),
+			}
+			*names = append(*names, tmp)
+		}
+		if info.IsDir() && path != root {
+			tmp := wikiNav{
+				Name: strings.Replace(path, root, "", 1) + "/",
+			}
+			*names = append(*names, tmp)
+		}
+		return nil
+	}
+
+}
+func walkWikiDir(path string) []wikiNav {
+	var names []wikiNav
+
+	err := filepath.Walk(path, closureProcess(path, &names))
+	checkErr(err)
+
+	return names
+
+}
+
+func getWikiList(root, path string) []wikiNav {
+	files, err := ioutil.ReadDir(path)
+	checkErr(err)
+
+	sort.Sort(sort.Reverse(byModTime(files)))
+
+	var names []wikiNav
+	for _, info := range files {
+		if info.IsDir() && contains(info.Name(), specialDir) {
+			continue
+		}
+		// Ignore anything that isnt an md file
+		if strings.HasSuffix(info.Name(), ".md") {
+			tmp := wikiNav{
+				Name: strings.TrimSuffix(info.Name(), ".md"),
+			}
+			names = append(names, tmp)
+		}
+		if strings.HasSuffix(info.Name(), ".pdf") {
+			tmp := wikiNav{
+				Name: info.Name(),
+			}
+			names = append(names, tmp)
+		}
+		if info.IsDir() {
+			tmp := wikiNav{
+				Name:  info.Name() + "/",
+				IsDir: true,
+			}
+			tmp.SubNav = getWikiList(root, path+"/"+info.Name())
+			names = append(names, tmp)
+		}
+	}
+
+	return names
+
+}
+
+func getNav() nav {
+	return nav{
+		Wikis: getWikiList(wikiDir, wikiDir),
+		Tags:  IndexRawFiles(wikiDir, "PDF", IndexTags(tagDir)),
+	}
+}
