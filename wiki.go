6c6bafa183108b0156cc0eba0eadec3fb6e3e138
Alan Gawthorpe <agawthorpe@gmail.com>
2020-11-09 20:25:57 +0000

    Remove rake - the summariser of wiki pages

--- wiki.go
+++ wiki.go
@@ -16,8 +16,6 @@ import (
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
-
-	rake "github.com/afjoseph/RAKE.Go"
 )
 
 var wikiDir string
@@ -116,13 +114,16 @@ func convertMarkdown(page *wikiPage, err error) (*wikiPage, error) {
 	if err != nil {
 		return page, err
 	}
-	index := rake.RunRake(string(page.Body))
-	for _, v := range index {
-		if v.Value < 4 {
-			break
-		}
-		page.Index = append(page.Index, v.Key)
-	}
+
+	// Used to run rake which was a summarizer for the page...however, it was a bit
+	// rubbish so have abandoned it.
+	// index := rake.RunRake(string(page.Body))
+	// for _, v := range index {
+	// 	if v.Value < 4 {
+	// 		break
+	// 	}
+	// 	page.Index = append(page.Index, v.Key)
+	// }
 
 	p := bluemonday.UGCPolicy()
 	p.AllowAttrs("class").Matching(regexp.MustCompile("^language-[a-zA-Z0-9]+$")).OnElements("code")
